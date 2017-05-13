6723937e6e4288d622b95cb2936c83b185be695e
Kazuyuki Iijima <iijimakazuyuki@gmail.com>
2017-05-14 00:03:20 +0900

    Add features for setting a random value and displaying that value

--- src/flag.js
+++ src/flag.js
@@ -3,8 +3,15 @@
  */
 
 class Flag {
-    constructor(value, display, target) {
-        this.value = value;
+    constructor(name, value, display, target) {
+        this.name = name;
+        if (typeof value === 'object') {
+            if (value.type === 'random') {
+                this.value = RANDOM_BETWEEN(value.min, value.max);
+            }
+        } else {
+            this.value = value;
+        }
         if (display) {
             this.display = display;
         } else {
@@ -15,5 +22,8 @@ class Flag {
         }
     }
 }
+
+const RANDOM_BETWEEN = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
 
 module.exports = Flag;
